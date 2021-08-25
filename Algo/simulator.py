2d7d149f873b9bff18f0cf499bec944cd0867af1
Jet <zhenjielau99@gmail.com>
2021-08-25 20:52:10 +0800

    calDIst Done

--- Algo/simulator.py
+++ Algo/simulator.py
@@ -14,7 +14,11 @@ NORTH = math.pi/2
 SOUTH = -math.pi/2
 EAST = 0
 WEST = -math.pi
-OBSTACLE = []
+OBSTACLES = [{"obstacleID": 1, "xPos": 1, "yPos": 1, "direction": NORTH, "distance": 0}, 
+            {"obstacleID": 1, "xPos": 9, "yPos": 5, "direction": NORTH, "distance": 0},
+            {"obstacleID": 1, "xPos": 2, "yPos": 7, "direction": NORTH, "distance": 50}]
+            #in the format [{obstacleID: 1, xPos: 1, yPos: 1, direction: NORTH, distance: 0}, {}...]
+            #values here just for testing and debugging 
 
 #input
 #stub method to get obstacle on terminal
@@ -27,9 +31,16 @@ def getObstacle():
         val = input("Enter next coordinate (enter END to end):\n")
 
 #find distance from car to obstacles
-def calculateDistance(OBSTACLE):
+#robotCurrPos is a dictionary {xPos: 1, yPos: 1, direction: NORTH}
+def calculateDistance(OBSTACLES, robotCurrPos):
+    for item in OBSTACLES:
+        dist = math.sqrt(((abs(item["xPos"]-robotCurrPos["xPos"]))**2) + ((abs(item["yPos"]-robotCurrPos["yPos"]))**2))
+        item["distance"] = dist
+    return OBSTACLES
 
+def nearestNeighbour(OBSTACLES):
+    return
 
-def nearestNeighbour(OBSTACLE):
-
-
+#just testing and debugging
+ans = calculateDistance(OBSTACLES, {"xPos": 10, "yPos": 10, "direction": NORTH})
+print(ans)
